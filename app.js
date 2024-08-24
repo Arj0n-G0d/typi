7637d85713ba0ebfe048c4768c904c539aecff11
Arj0n-G0d <gaurxarjun@gmail.com>
2024-08-24 18:07:04 +0530

    MODIFY - Experimentation Files 1) Change of color scheme 2) Optimized logic for keystrokes for smoother animation

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
-function keyPress(c) {
+function keyPressDown(c) {
     let id="#"+c.toUpperCase();
     console.log(id);
     const keyy=document.querySelector(id);
     keyy.classList.add("keyPress");
-    setTimeout(()=>{
-        keyy.classList.remove("keyPress");
-    },100);
+}
+
+function keyPressUp(c) {
+    let id="#"+c.toUpperCase();
+    console.log(id);
+    const keyy=document.querySelector(id);
+    keyy.classList.remove("keyPress");
 }
 
 const key=document.querySelector("#keyy");
 document.body.addEventListener("keydown",(e)=>{
     
     if((e.key>="A" && e.key<="Z") || (e.key>="a" && e.key<="z")) {
-        keyPress(e.key); 
+        keyPressDown(e.key); 
     }
     else if(e.code=="Space") {
-        keyPress("space");
+        keyPressDown("space");
     }
-})
+});
+document.body.addEventListener("keyup",(e)=>{
+    
+    if((e.key>="A" && e.key<="Z") || (e.key>="a" && e.key<="z")) {
+        keyPressUp(e.key); 
+    }
+    else if(e.code=="Space") {
+        keyPressUp("space");
+    }
+});
