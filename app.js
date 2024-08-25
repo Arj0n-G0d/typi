edb91004dc65844a35680d1663041cac12d750ea
Arj0n-G0d <gaurxarjun@gmail.com>
2024-08-25 23:29:59 +0530

    MODIFY - app.css, app.js 1) Added Game class 2) Cached Key Elements

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
-let string="Hello My name is Arjun Gaur";
+let game;
 let pressed={};
-let cursor;
+let keys={};
+for(let i=65;i<=90;i++)
+    keys[String.fromCharCode(i)]=document.querySelector("#"+String.fromCharCode(i));
+keys[" "]=document.querySelector("#SPACE");
+keys["back"]=document.querySelector("#BACKSPACE");
 
-function keyPress(id) {
-    const key=document.querySelector(id);
-    key.classList.toggle("keyPress");
+class Game {
+    constructor() {
+        this.string="Lorem ipsum dolor sit amet consectetur adipisicing elit Cum ipsa architecto inventore unde molestias dolor enim";
+        this.addString();
+        this.cursorIndex=0;
+        this.cursorArray=document.getElementsByClassName("letter");
+        this.cursorArray[this.cursorIndex].classList.add("cursor");
+        this.lastCursorIndex=0;
+        this.thresh=25;
+    }
+    addString() {
+        const reader=document.querySelector(".reader");
+        reader.textContent="";
+        for(let i=0;i<this.string.length;i++) {
+            const letter=document.createElement("span");
+            letter.classList.add("letter");
+            letter.textContent=this.string[i];
+            reader.appendChild(letter);
+        }
+    }
 }
 
 function typeTypi() {
@@ -19,68 +40,66 @@ function typeTypi() {
         clearInterval(timer);
     },2000);
     setTimeout(()=>{
-        reader.textContent="";
         reader.classList.toggle("typi");
     },3000);
 }
 
-function addTest(str) {
-    const reader=document.querySelector(".reader");
-    for(let i=0;i<str.length;i++) {
-        const elmnt=document.createElement("span");
-        elmnt.classList.add("letter");
-        elmnt.textContent=str[i];
-        reader.appendChild(elmnt);
-    }
-    cursor=document.querySelector(".letter");
-    cursor.classList.add("cursor");
-}
-
 typeTypi();
 setTimeout(()=>{
-    addTest(string);
+    game=new Game();
 },3000);
 
 document.body.addEventListener("keydown",(e)=>{
     if(!pressed[e.code]) {
-        console.log(e.key);
         let letter=null;
         if(e.key.length==1 && ((e.key>="A" && e.key<="Z") || (e.key>="a" && e.key<="z"))) {
-            keyPress("#"+e.key.toUpperCase()); 
+            keys[e.key.toUpperCase()].classList.toggle("keyPress"); 
             pressed[e.code]=true;
             letter=e.key;
         }
         else if(e.key==" ") {
-            keyPress("#SPACE");
+            keys[" "].classList.toggle("keyPress"); 
             pressed[e.code]=true;
             letter=" ";
         }
         else if(e.key=="Backspace") {
-            keyPress("#BACKSPACE");
+            keys["back"].classList.toggle("keyPress"); 
             pressed[e.code]=true;
             letter="back";
         }
-        if(letter==cursor.textContent) {
-            cursor.classList.add("correct");
-            cursor.classList.remove("cursor");
-            cursor=cursor.nextSibling;
-            if(cursor!=null)
-                cursor.classList.add("cursor");
+        if(letter!=null && letter==game.cursorArray[game.cursorIndex].textContent) {
+            game.cursorArray[game.cursorIndex].classList.add("correct");
+            game.cursorArray[game.cursorIndex].classList.remove("cursor");
+            game.cursorIndex++;
+            if(game.cursorArray[game.cursorIndex]!=undefined)
+                game.cursorArray[game.cursorIndex].classList.add("cursor");
+            if(game.cursorIndex-game.lastCursorIndex>=game.thresh && game.cursorArray.length-game.cursorIndex>=game.thresh) {
+                game.cursorArray[game.lastCursorIndex].style.display="none";
+                game.lastCursorIndex++;
+            }
         }
         else if(letter!=null && letter!="back"){
-            cursor.classList.add("incorrect");
-            cursor.classList.remove("cursor");
-            cursor=cursor.nextSibling;
-            if(cursor!=null)
-                cursor.classList.add("cursor");
+            game.cursorArray[game.cursorIndex].classList.add("incorrect");
+            game.cursorArray[game.cursorIndex].classList.remove("cursor");
+            game.cursorIndex++;
+            if(game.cursorArray[game.cursorIndex]!=undefined)
+                game.cursorArray[game.cursorIndex].classList.add("cursor");
+            if(game.cursorIndex-game.lastCursorIndex>=game.thresh) {
+                game.cursorArray[game.lastCursorIndex].style.display="none";
+                game.lastCursorIndex++;
+            }
         }
         else if(letter!=null && letter=="back") {
-            let prev=cursor.previousSibling;
-            if(prev && prev.classList.contains("incorrect")) {
-                cursor.classList.remove("cursor");
-                prev.classList.remove("incorrect");
-                cursor=prev;
-                cursor.classList.add("cursor");
+            let prev=game.cursorArray[game.cursorIndex-1];
+            if(prev) {
+                game.cursorArray[game.cursorIndex].classList.remove("cursor");
+                prev.classList.remove("incorrect","correct"); 
+                game.cursorIndex--;
+                game.cursorArray[game.cursorIndex].classList.add("cursor");
+            }
+            if(game.cursorIndex-game.lastCursorIndex<game.thresh) {
+                game.cursorArray[game.lastCursorIndex-1].style.display="inline";
+                game.lastCursorIndex--;
             }
         }
     }
@@ -89,15 +108,15 @@ document.body.addEventListener("keydown",(e)=>{
 document.body.addEventListener("keyup",(e)=>{
     if(pressed[e.code]) {
         if(e.key.length==1 && ((e.key>="A" && e.key<="Z") || (e.key>="a" && e.key<="z"))) {
-            keyPress("#"+e.key.toUpperCase()); 
+            keys[e.key.toUpperCase()].classList.toggle("keyPress"); 
             pressed[e.code]=false;
         }
         else if(e.key==" ") {
-            keyPress("#SPACE");
+            keys[" "].classList.toggle("keyPress"); 
             pressed[e.code]=false;
         }
         else if(e.key=="Backspace") {
-            keyPress("#BACKSPACE");
+            keys["back"].classList.toggle("keyPress"); 
             pressed[e.code]=false;
         }
     }
