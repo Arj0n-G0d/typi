71a58ad372660f1f03a90194a6dd25f28254ba82
Arj0n-G0d <gaurxarjun@gmail.com>
2024-08-27 00:28:45 +0530

    MODIFY - All Files 1) Added Keyboard Dimming and Lightening up on Game start and end

--- app.js
+++ app.js
@@ -4,32 +4,57 @@ let keys={};
 for(let i=65;i<=90;i++)
     keys[String.fromCharCode(i)]=document.querySelector("#"+String.fromCharCode(i));
 keys[" "]=document.querySelector("#SPACE");
-keys["back"]=document.querySelector("#BACKSPACE");
+keys["backspace"]=document.querySelector("#BACKSPACE");
 
 class Game {
     constructor() {
-        this.string="Lorem ipsum dolor sit amet consectetur adipisicing elit Cum ipsa architecto inventore unde molestias dolor enim";
+        this.string="Lorem ipsum dolor";
         this.addString();
         this.cursorIndex=0;
         this.cursorArray=document.getElementsByClassName("letter");
         this.cursorArray[this.cursorIndex].classList.add("cursor");
         this.lastCursorIndex=0;
         this.thresh=25;
+        this.result=document.querySelector(".resultAvg");
+        this.result.style.display="none";
+
+        for(let key in keys)
+            keys[key].classList.add("appearFromDimAnimation");
+        const keyboard=document.querySelector(".keyboard");
+        keyboard.classList.add("appearFromDimAnimationKeyboard");
     }
     addString() {
-        const reader=document.querySelector(".reader");
+        const reader=document.querySelector(".readerOrig");
         reader.textContent="";
         for(let i=0;i<this.string.length;i++) {
             const letter=document.createElement("span");
             letter.classList.add("letter");
-            letter.textContent=this.string[i];
+            if(this.string[i]!=" ")
+                letter.textContent=this.string[i];
+            else
+                letter.textContent="\u00A0";
             reader.appendChild(letter);
         }
+        reader.classList.remove("readerOrig");
+    }
+    gameOver() {
+        this.result.style.display="block";
+        this.result.classList.add("resultAfter");
+        
+        for(let key in keys) {
+            keys[key].classList.remove("appearFromDimAnimation");
+            void keys[key].offsetWidth;
+            keys[key].classList.add("disappearToDimAnimation");
+        }
+        const keyboard=document.querySelector(".keyboard");
+        keyboard.classList.remove("appearFromDimAnimationKeyboard");
+        void keyboard.offsetWidth;
+        keyboard.classList.add("disappearToDimAnimationKeyboard");
     }
 }
 
 function typeTypi() {
-    const reader=document.querySelector(".reader");
+    const reader=document.querySelector(".typi");
     console.log(reader);
     const typi="TYPI";
     let cnt=0;
@@ -40,8 +65,10 @@ function typeTypi() {
         clearInterval(timer);
     },2000);
     setTimeout(()=>{
-        reader.classList.toggle("typi");
-    },3000);
+        reader.classList.remove("typi");
+        reader.textContent="Press Enter to Start";
+        reader.classList.add("readerOrig");
+    },2500);
 }
 
 typeTypi();
@@ -63,7 +90,7 @@ document.body.addEventListener("keydown",(e)=>{
             letter=" ";
         }
         else if(e.key=="Backspace") {
-            keys["back"].classList.toggle("keyPress"); 
+            keys["backspace"].classList.toggle("keyPress"); 
             pressed[e.code]=true;
             letter="back";
         }
@@ -102,6 +129,10 @@ document.body.addEventListener("keydown",(e)=>{
                 game.lastCursorIndex--;
             }
         }
+        if(game.cursorIndex>=game.cursorArray.length) {
+            console.log("hello");
+            game.gameOver();
+        }
     }
 });
 
@@ -116,7 +147,7 @@ document.body.addEventListener("keyup",(e)=>{
             pressed[e.code]=false;
         }
         else if(e.key=="Backspace") {
-            keys["back"].classList.toggle("keyPress"); 
+            keys["backspace"].classList.toggle("keyPress"); 
             pressed[e.code]=false;
         }
     }
