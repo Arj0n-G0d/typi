a1a45720aef07c8cc6bc53c37339b9b043a38f54
Arj0n-G0d <gaurxarjun@gmail.com>
2024-09-01 09:18:12 +0530

    MODIFY - All Files 1) Reader displays "Press ENTER to start" before and after a game 2) New game starts when user presses ENTER 3) Integrated Random-Word-API 4) Backspace now alters game.incorrect and game.correct leading to accurate speed measurements

--- app.js
+++ app.js
@@ -7,25 +7,22 @@ keys[" "]=document.querySelector("#SPACE");
 keys["backspace"]=document.querySelector("#BACKSPACE");
 
 class Game {
-    constructor() {
-        this.string="Lorem ipsum dolor";
-        this.addString();
+    constructor(str) {
         this.cursorIndex=0;
+        this.length=25;
+        this.string=str;
+        this.addString();
         this.cursorArray=document.getElementsByClassName("letter");
         this.cursorArray[this.cursorIndex].classList.add("cursor");
         this.lastCursorIndex=0;
-        this.thresh=25;
-
-        for(let key in keys) {
-            keys[key].classList.remove("disappearToDimAnimation");
-            void keys[key].offsetWidth;
-            keys[key].classList.add("appearFromDimAnimation");
-        }
-        const keyboard=document.querySelector(".keyboard");
-        keyboard.classList.remove("disappearToDimAnimationKeyboard");
-        void keyboard.offsetWidth;
-        keyboard.classList.add("appearFromDimAnimationKeyboard");
-
+        this.thresh=10;
+        this.start=new Date();
+        this.end;
+        this.correct=0;
+        this.incorrect=0;
+        this.curr;
+        this.max=0;  
+        
         this.resultAvg=document.querySelector(".resultAvg");
         this.resultMax=document.querySelector(".resultMax");
 
@@ -34,12 +31,11 @@ class Game {
         this.resultAvg.classList.add("disappearToDark");  
         this.resultMax.classList.remove("appearFromDark");
         void this.resultMax.offsetWidth;
-        this.resultMax.classList.add("disappearToDark");     
-    }
+        this.resultMax.classList.add("disappearToDark");
+    }   
     addString() {
         const reader=document.querySelector(".readerOrig");
         reader.classList.remove("readerOrig");
-        reader.textContent="";
         for(let i=0;i<this.string.length;i++) {
             const letter=document.createElement("span");
             letter.classList.add("letter");
@@ -67,16 +63,24 @@ class Game {
         reader.classList.remove("reader");
         void reader.offsetWidth;
         reader.classList.add("readerOrig");
-        reader.textContent="Press Enter to Start";
+        reader.innerHTML='Press <span style="color:#fca311">ENTER</span> to Start';
 
-        this.resultAvg.style.display="block";
-        this.resultMax.style.display="block";
+        this.resultAvg.style.display="flex";
+        this.resultMax.style.display="flex";
         this.resultAvg.classList.remove("disappearToDark");
         void this.resultAvg.offsetWidth;
         this.resultAvg.classList.add("appearFromDark");
         this.resultMax.classList.remove("disappearToDark");
         void this.resultMax.offsetWidth;
-        this.resultMax.classList.add("appearFromDark");       
+        this.resultMax.classList.add("appearFromDark");  
+        
+        this.end=new Date();
+        let duration=(this.end.getTime()-this.start.getTime())/1000;
+        const avg=document.querySelector(".avgSpeed");
+        avg.textContent=Math.floor(this.correct/duration*12);
+
+        const max=document.querySelector(".maxSpeed");
+        max.textContent=game.max;
     }
 }
 
@@ -94,14 +98,11 @@ function typeTypi() {
     setTimeout(()=>{
         reader.classList.remove("typi");
         reader.classList.add("readerOrig");
-        reader.textContent="Press Enter to Start";
+        reader.innerHTML='Press <span style="color:#fca311">ENTER</span> to Start';
     },2500);
 }
 
 typeTypi();
-setTimeout(()=>{
-    game=new Game();
-},5000);
 
 document.body.addEventListener("keydown",(e)=>{
     if(!pressed[e.code]) {
@@ -131,6 +132,11 @@ document.body.addEventListener("keydown",(e)=>{
                 game.cursorArray[game.lastCursorIndex].style.display="none";
                 game.lastCursorIndex++;
             }
+            game.correct++;
+
+            game.curr=new Date();
+            let duration=(game.curr.getTime()-game.start.getTime())/1000;
+            game.max=Math.max(game.max,Math.floor(game.correct/duration*12));
         }
         else if(letter!=null && letter!="back"){
             game.cursorArray[game.cursorIndex].classList.add("incorrect");
@@ -147,7 +153,14 @@ document.body.addEventListener("keydown",(e)=>{
             let prev=game.cursorArray[game.cursorIndex-1];
             if(prev) {
                 game.cursorArray[game.cursorIndex].classList.remove("cursor");
-                prev.classList.remove("incorrect","correct"); 
+                if(prev.classList.contains("incorrect")) {
+                    prev.classList.remove("incorrect");
+                    game.incorrect--;
+                }
+                else if(prev.classList.contains("correct")) {
+                    prev.classList.remove("correct");
+                    game.correct--;
+                } 
                 game.cursorIndex--;
                 game.cursorArray[game.cursorIndex].classList.add("cursor");
             }
@@ -156,9 +169,39 @@ document.body.addEventListener("keydown",(e)=>{
                 game.lastCursorIndex--;
             }
         }
-        if(game.cursorIndex>=game.cursorArray.length) {
+        else if(e.code=="Enter") {
+            if(game==undefined) {
+                const reader=document.querySelector(".readerOrig");
+                reader.textContent="";
+                let size=Math.floor(Math.random()*25);
+                axios.get("https://random-word-api.herokuapp.com/word?number="+size).then((res)=>{
+                    let string="";
+                    const wordsArray=res.data;
+                    for(let i=0;i<size;i++) {
+                        if(string=="") string+=wordsArray[i];
+                        else string+=" "+wordsArray[i];
+                    }
+                    for(let key in keys) {
+                        keys[key].classList.remove("disappearToDimAnimation");
+                        void keys[key].offsetWidth;
+                        keys[key].classList.add("appearFromDimAnimation");
+                    }
+                    const keyboard=document.querySelector(".keyboard");
+                    keyboard.classList.remove("disappearToDimAnimationKeyboard");
+                    void keyboard.offsetWidth;
+                    keyboard.classList.add("appearFromDimAnimationKeyboard");
+                    game=new Game(string);
+                });
+            }
+        }
+        else {
+            game.incorrect++;
+        }
+        if(game!=undefined && game.cursorIndex>=game.cursorArray.length) {
             console.log("hello");
-            game.gameOver();
+            if(game!=undefined)
+                game.gameOver();
+            game=undefined;
         }
     }
 });
